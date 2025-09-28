e5332512ad7ada75fa30b2c056c1b0d624bad179
RAMSEY <r80128485@gmail.com>
2025-09-28 13:44:26 +0000

    Mise a jour du depot

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+// https://vite.dev/config/
+export default defineConfig({
+    server: {
+        host: "localhost",
+        port: 5173,
+        hmr: {
+            overlay: false,
+        },
+    },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
